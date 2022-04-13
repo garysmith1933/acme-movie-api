4dadf7fc258fd9e50b798b5c105aecfd357cebf4
Gary Smith <garysmith193358@gmail.com>
2022-04-13 00:26:08 +0000

    routes passed

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require("express")
 const app = express()
 const path = require('path')
+const {models} = require("./db")
+const {Movie} = models
 
 app.get("/", (req,res) => res.sendFile(path.join(__dirname, 'index.html')))
+
+app.get('/api/movies', async(req, res, next)=> {
+  try {
+    res.send(await Movie.findAll());
+  }
+  catch(ex){
+    next(ex);
+  }
+});
 module.exports = app;
--- test/spec.js
+++ test/spec.js
@@ -1,4 +1,5 @@
 const {expect } = require("chai")
+const {syncAndSeed} = require("../db")
 const supertest = require("supertest")
 const app = supertest(require("../app"))
 
@@ -15,3 +16,15 @@ describe('The Home Page', ()=> {
         expect(response.text).to.contain('The Acme Movies Api')
     })
 })
+
+describe('/api routes', ()=> {
+  beforeEach(()=> syncAndSeed());
+  describe('/api/movies', ()=> {
+    it('returns all the movies', async()=> {
+      const response = await app.get('/api/movies');
+      expect(response.status).to.equal(200);
+      expect(response.body.length).to.equal(4);
+
+    });
+  });
+});
